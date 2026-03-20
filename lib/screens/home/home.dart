23f2c0aca9915b1f44eeec24019f2d0dcfc250e2
tiaraeshwara <tiaraeshwara320@gamil.com>
2026-03-20 12:38:01 +0530

    created home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HOME"),
+      )),
+      );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_app/screens/authentication/authenticate.dart';
 import 'package:firebase_auth_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Authenticate(),
-    );
+    final user = Provider.of<User?>(context);
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+    
   }
 }
